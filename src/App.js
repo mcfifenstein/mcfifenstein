badee4ff8653dc7c8286896a3f849d32b785f5a7
Joe Fifer <jfifer@baseninestudios.com>
2021-04-20 15:19:14 -0400

    Testing CI

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://zombo.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          You can do anything at Zombocom
         </a>
       </header>
     </div>
